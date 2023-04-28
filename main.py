e057450389a74e9b7b278ff09a4efdfb7b6dcbb0
Mindaugas <mindaugas.antanas.grigaitis@gmail.com>
2023-04-28 17:15:36 +0300

    Rewrote the code to use Queue

--- main.py
+++ main.py
@@ -18,27 +18,33 @@ To choose either of the 2, enter 1 or 2 respectively or enter a custom amount
 import socket
 import threading
 import time
+from queue import Queue
+
+q = Queue()
 
 # Initializes the app again once finished
 while 1:
     ADDRESS = str(input("Enter IP or Domain address: "))
     num_of_ports = int(input("Enter the amount of ports (defaults are: 1. WKP: 0 - 1023, 2. RP: 1024 - 49151): "))
-    timeout = int(input("Seconds before connection times out (default is 5): ") or "5")
+    timeout = int(input("Seconds before connection times out (default is 3): ") or "3")
 
-    # Thread range loop start
-    range_start = 0
+    # Range loop starting integer
+    num_of_ports_start = 0
 
     # Amount of ports (to and from) based on num_of_ports input (1/2)
     if num_of_ports == 1:
         num_of_ports = 1024
     elif num_of_ports == 2:
-        range_start = 1024
+        num_of_ports_start = 1024
         num_of_ports = 49152
 
     finish = None
-    current_port = 0
     open_ports = []
     threads = []
+
+    # Queues ports
+    for i in range(num_of_ports_start, num_of_ports):
+        q.put(i)
 
     scanning_text = f"> Scanning {ADDRESS}"
 
@@ -58,27 +64,33 @@ while 1:
                 scanning_text = scanning_text[:-4]
                 counter = 0
 
-
     text_dot_thread = threading.Thread(target=text_dots)
     text_dot_thread.start()
 
     start = time.time()
 
-    def port_scanner(current_port):
+    def port_scanner(port):
         with socket.socket() as s:
             s.settimeout(timeout)
 
             try:
-                s.connect((ADDRESS, current_port))
-                open_ports.append(current_port)
+                s.connect((ADDRESS, port))
+                open_ports.append(port)
             except:
                 pass
 
-    for _ in range(range_start, num_of_ports):
-        thread = threading.Thread(target=port_scanner, args=(current_port,))
-        thread.start()
-        threads.append(thread)
-        current_port += 1
+    def worker():
+        while not q.empty():
+            port = q.get()
+            port_scanner(port)
+
+    def threader(start, end):
+        for _ in range(start, end):
+            thread = threading.Thread(target=worker)
+            thread.start()
+            threads.append(thread)
+
+    threader(num_of_ports_start, num_of_ports)
 
     # Waits for all threads to finish before outputting the result
     for thread in threads:
